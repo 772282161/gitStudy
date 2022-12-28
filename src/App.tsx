cb23d7e9949795ecf33489007562c7363b0fe0b0
支恩泽 <772282161@qq.com>
2022-12-28 15:25:33 +0800

    Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import React,{useState} from 'react'
-import '@/app.css'
-import '@/app.less'
+import Home from '@/pages/Home'
+import { Header } from 'antd/es/layout/layout'
+
 function App() {
-    const [count,setCount] = useState('')
-    const onChange = (e:any) => {
-        setCount(e.target.value)
-    }
 
     return(
-        <>
-            <h2>webpack5+react+ts</h2>
-            <p>受控组件</p>
-            <input type="text" value={count} onChange={onChange} />
-            <br />
-            <p>非受控组件</p>
-            <input type="text" />
-            <h2> 我是修改后的组件</h2>
-        </>
+        <div>
+            <div>App入口</div>
+            <Header />
+        </div>
     )
 }
 
---
+++ src/pages/Home/Header/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {DownOutlined} from '@ant-design/icons'
+import {Dropdown, Space} from 'antd'
+import type { MenuProps } from "rc-menu";
+
+const Header = () => {
+  
+};
+
+export default Header;
